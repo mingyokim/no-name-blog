afd215e17cc5383d9e0f7a9aa2b3869265607a03
Min Gyo Kim <mingyo.kim3@gmail.com>
2020-06-05 00:18:33 -0700

    Change bg color to black

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { renderRoutes } from 'react-router-config';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
 import routes from '../../routes';
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      default: '#131415',
+    }
+  }
+});
 
 export default function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       {renderRoutes(routes)}
-    </div>
+    </ThemeProvider>
   );
 }
