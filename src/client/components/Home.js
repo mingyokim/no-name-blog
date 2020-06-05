c18698ef2c591d842ef1ab76fbd85c2e11b7bb04
Min Gyo Kim <mingyo.kim3@gmail.com>
2020-06-04 21:32:58 -0700

    Clear root

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { on: false };
+  }
+
+  render() {
+    const { on } = this.state;
+
+    const toggle = () => {
+      // const { on } = this.state;
+      this.setState({ on: !on });
+    };
+
+    if (on) {
+      return (
+        <>
+          <button onClick={toggle}>Button</button>
+          <p>on</p>
+        </>
+      );
+    }
+    return (
+      <>
+        <button onClick={toggle}>Button</button>
+        <p>off</p>
+      </>
+    );
+  }
+}
+
 const Home = () => (
-  <h2>Home</h2>
+  <>
+    <h2>Home</h2>
+    <Toggle />
+  </>
 );
 
 export default Home;
--- src/client/components/Root.js
+++ src/client/components/Root.js
@@ -2,41 +2,8 @@ import React from 'react';
 import { renderRoutes } from 'react-router-config';
 import PropTypes from 'prop-types';
 
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { on: false };
-  }
-
-  render() {
-    const { on } = this.state;
-
-    const toggle = () => {
-      // const { on } = this.state;
-      this.setState({ on: !on });
-    };
-
-    if (on) {
-      return (
-        <>
-          <button onClick={toggle}>Button</button>
-          <p>on</p>
-        </>
-      );
-    }
-    return (
-      <>
-        <button onClick={toggle}>Button</button>
-        <p>off</p>
-      </>
-    );
-  }
-}
-
 const Root = ({ route }) => (
   <div>
-    <h1>Root</h1>
-    <Toggle />
     {renderRoutes(route.routes)}
   </div>
 );
