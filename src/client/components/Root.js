28cf4be2bf7cfa8422344dccbcffebcb19e72f38
Min Gyo Kim <mingyo.kim3@gmail.com>
2020-07-16 20:54:44 -0700

    Use container max width instead of percentage paddings

--- src/client/components/Root.js
+++ src/client/components/Root.js
@@ -4,15 +4,9 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   root: {
     padding: '40px 28px 40px 28px',
-    [theme.breakpoints.up('md')]: {
-      padding: '40px 20% 40px 20%',
-    },
-    [theme.breakpoints.up('lg')]: {
-      padding: '40px 30% 40px 30%',
-    },
     height: '100vh'
   },
 }));
@@ -20,7 +14,7 @@ const useStyles = makeStyles(theme => ({
 const Root = ({ route }) => {
   const classes = useStyles();
   return (
-    <Container className={classes.root} maxWidth={false}>
+    <Container className={classes.root} maxWidth="md">
       {renderRoutes(route.routes)}
     </Container>
   );
