3aa890396392a20702afcf2e6f3683a4f26076ec
Min Gyo Kim <mingyo.kim3@gmail.com>
2020-07-21 16:00:18 -0700

    Added app to functions

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,6 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const app = require('./build/server').default;
+exports.app = functions.https.onRequest(app);
