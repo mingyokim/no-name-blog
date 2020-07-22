3fcd1753b7eff742a822c569c71b5d2abb38c899
Min Gyo Kim <mingyo.kim3@gmail.com>
2020-07-22 00:07:25 -0700

    try to get firebase client config to work

--- predeploy.sh
+++ predeploy.sh
@@ -1,1 +1,5 @@
-npm run build
+# echo $FB_CLIENT
+# echo 'predeploy!'
+npm run build:firebase
+# cp -r ./public ./functions/public
+# cp -r ./dist ./functions/dist
--- src/client/firebase/index.js
+++ src/client/firebase/index.js
@@ -3,7 +3,12 @@ import 'firebase/auth';
 
 // console.log(FB_CLIENT);
 // console.log(JSON.parse(FB_CLIENT));
-
-firebase.initializeApp(JSON.parse(FB_CLIENT));
+try {
+  firebase.initializeApp(JSON.parse(FB_CLIENT));
+} catch (err) {
+  fetch('/__/firebase/init.json').then(async response => {
+    firebase.initializeApp(await response.json());
+  });
+}
 
 export default firebase;
