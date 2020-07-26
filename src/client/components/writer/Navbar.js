705aa7cca664ffd111a01b929aac172391d4ac20
Min Gyo Kim <mingyo.kim3@gmail.com>
2020-07-26 12:53:23 -0700

    navbarrrrr

--- src/client/components/writer/Navbar.js
+++ src/client/components/writer/Navbar.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link, useLocation } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import NavigateBeforeRoundedIcon from '@material-ui/icons/NavigateBeforeRounded';
 
 const Navbar = ({
   author: {
     photoURL,
   }
-}) => (
-  <Grid justify="flex-end" container>
+}) => {
+  const {
+    pathname
+  } = useLocation();
+  const isWriterHome = pathname === '/writer';
+  return (
+    <Grid justify={isWriterHome ? 'flex-end' : 'space-between'} container>
+      {isWriterHome ?
+        null :
+        (
+          <Grid item>
+            <IconButton
+              aria-label="back"
+              component={Link}
+              to="/writer"
+            >
+              <NavigateBeforeRoundedIcon fontSize="large" />
+            </IconButton>
+          </Grid>
+        )
+      }
       <Grid item>
         <Avatar src={photoURL} />
       </Grid>
     </Grid>
-);
+  )
+};
 
 Navbar.propTypes = {
   author: PropTypes.shape({
